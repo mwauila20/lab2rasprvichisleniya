d1d1ae56f9ecd3a3982ef64798c156c696ece4c1
mwauila20 <90110442+mwauila20@users.noreply.github.com>
2024-04-23 22:17:27 +0300

    Add files via upload

---
+++ lab2/Lab2/Lab2.cpp
@@ -0,0 +1,165 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#pragma comment(lib, "ws2_32.lib")
+
+
+
+int main() {
+    double accumulatedResult = 0.0;
+    WSADATA wsaData;
+    setlocale(LC_ALL, "en_us");
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+
+    int port;
+    std::cout << "Enter server port: ";
+    std::cin >> port;
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(port);
+
+    if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Bind failed\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "Listen failed\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server listening on port " << port << "\n";
+
+    std::vector<SOCKET> clients;
+    fd_set readSet;
+    char buffer[1024];
+    int bytesRead;
+
+    while (true) {
+        FD_ZERO(&readSet);
+        FD_SET(serverSocket, &readSet);
+
+        for (const auto& client : clients) {
+            FD_SET(client, &readSet);
+        }
+
+        int activity = select(0, &readSet, nullptr, nullptr, nullptr);
+        if (activity == SOCKET_ERROR) {
+            std::cerr << "Select failed\n";
+            break;
+        }
+
+        if (FD_ISSET(serverSocket, &readSet)) {
+            // New connection
+            SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+            if (clientSocket == INVALID_SOCKET) {
+                std::cerr << "Accept failed\n";
+                break;
+            }
+
+            clients.push_back(clientSocket);
+
+            char clientIP[INET_ADDRSTRLEN];
+            sockaddr_in clientAddr;
+            int clientAddrSize = sizeof(clientAddr);
+            getpeername(clientSocket, reinterpret_cast<sockaddr*>(&clientAddr), &clientAddrSize);
+            inet_ntop(AF_INET, &(clientAddr.sin_addr), clientIP, INET_ADDRSTRLEN);
+
+            std::cout << "Client " << clients.size() << " connected: " << clientIP << ":" << ntohs(clientAddr.sin_port) << "\n";
+        }
+
+        for (auto it = clients.begin(); it != clients.end(); ) {
+            SOCKET clientSocket = *it;
+
+            if (FD_ISSET(clientSocket, &readSet)) {
+                bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+                if (bytesRead > 0) {
+                    buffer[bytesRead] = '\0';
+                    std::cout << "Received from client " << std::distance(clients.begin(), it) + 1 << ": " << buffer << "\n";
+
+                    // Handle your logic here based on received data
+
+                    // For example, if received "Task"
+                    if (std::string(buffer) == "Task") {
+                        double start = 10.0;
+                        double end = 20.0;
+                        double step = 0.0000001;
+                        if (std::distance(clients.begin(), it) + 1 == 1)
+                        {
+                            start = 10;
+                            end = 15;
+                        }
+                        else {
+                            start = 15;
+                            end = 20;
+                        }
+                        // Convert double values to strings
+                        std::ostringstream startStr, endStr, stepStr;
+                        startStr << start;
+                        endStr << end;
+                        stepStr << step;
+
+                        std::string params = startStr.str() + " " + endStr.str() + " " + stepStr.str();
+
+                        std::cout << "Sending integral parameters to client " << std::distance(clients.begin(), it) + 1 << ": " << params << "\n";
+
+                        // Send the parameters to the client
+                        send(clientSocket, params.c_str(), params.size() + 1, 0);
+                    }
+                    if (strncmp(buffer, "Result-", 7) == 0) {
+                        // Extract the result from the message (skip "Result-" prefix)
+                        double result = std::stod(buffer + 7);
+
+                        std::cout << "Received integral result from client " << std::distance(clients.begin(), it) + 1 << ": " << result << "\n";
+
+                        // Accumulate the result received from the client
+                        accumulatedResult += result;
+                        std::cout << "Accumulated result: " << accumulatedResult << "\n";
+                        
+                    }
+                }
+                
+                else {
+                    // Connection closed by client
+                    std::cout << "Client " << std::distance(clients.begin(), it) + 1 << " disconnected\n";
+                    closesocket(clientSocket);
+                    it = clients.erase(it);
+                    continue;
+                }
+            }
+
+            ++it;
+        }
+    }
+
+    // Cleanup
+    for (const auto& client : clients) {
+        closesocket(client);
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ lab2/Lab2Client/Lab2Client.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+#include <sstream>
+#include <cmath>
+#pragma comment(lib, "ws2_32.lib")
+
+double Function(double x) {
+    return 1.0 / log(x);
+}
+
+// Функция для расчета интеграла
+double CalculateIntegral(double start, double end, double step) {
+    double sum = 0.0;
+    for (double x = start; x < end; x += step) {
+        sum += Function(x) * step;
+    }
+    return sum;
+}
+
+int main() {
+    WSADATA wsaData;
+    setlocale(LC_ALL, "en_us");
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+
+    std::string serverIP;
+    std::cout << "Enter server IP: ";
+    std::cin >> serverIP;
+
+    int port;
+    std::cout << "Enter server port: ";
+    std::cin >> port;
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, serverIP.c_str(), &(serverAddr.sin_addr));
+    serverAddr.sin_port = htons(port);
+
+    if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Connection failed\n";
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Connected to server\n";
+    while (true) {
+        std::string message;
+        std::cout << "Enter message (or 'exit' to quit): ";
+        std::getline(std::cin, message);
+
+        if (message == "exit") {
+            break;
+        }
+
+        send(clientSocket, message.c_str(), message.size(), 0);
+
+        char buffer[1024];
+
+        if (message == "Task") {
+            double start, end, step;
+            char paramsBuffer[1024];
+
+            // Receive the parameters from the server
+            recv(clientSocket, paramsBuffer, sizeof(paramsBuffer), 0);
+
+            // Parse the received string back to double values
+            std::istringstream paramsStream(paramsBuffer);
+            paramsStream >> start >> end >> step;
+
+            std::cout << "Received integral parameters from server: start=" << start << ", end=" << end << ", step=" << step << "\n";
+
+            double result = CalculateIntegral(start, end, step);
+
+            // Convert the result to a string before sending
+            std::ostringstream resultStr;
+            resultStr << "Result-" << result;
+            
+            send(clientSocket, resultStr.str().c_str(), resultStr.str().size() + 1, 0);
+        }
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
